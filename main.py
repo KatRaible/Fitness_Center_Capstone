f363205e5bca1a7742c07ca7d62223c98e218f32
Adriana Jeffries <adrianamjeffries@gmail.com>
2023-08-16 21:56:38 -0400

    End of class time

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def main():
         print("4. Check in Member")
         print("5. Display Bill")
         print("6. Exit")
+        print()
 
         choice = int(input("Enter your choice: "))
+        print()
 
         if choice == 1: 
             member_id = input("Enter Member ID: ")
@@ -35,9 +37,10 @@ def main():
                 members.append(MultiClubMember(member_id, name))
             else:
                 print("error")
+            print()
         elif choice == 2:  # remove member
             member_id = input("Enter member ID to remove: ")
-            print(len(members))
+            #print(len(members))
             for member in members:
                 if member_id == member.member_id:
                     members.remove(member)
@@ -45,8 +48,9 @@ def main():
                     break
             else:
                 print("Member not found.")
+            print()
         elif choice == 3:  # display member info
-            member_id = input("Enter member ID to display info: ")
+            member_id = input("\nEnter member ID to display info: ")
             for member in members:
                 if member.member_id == member_id:
                     print(f"Member ID: {member.member_id}")
@@ -58,6 +62,7 @@ def main():
                     break
             else:
                 print("Member not found.")
+            print()
         elif choice == 4: #check in member
             member_id = input("Enter member ID to check in: ")
             for member in members:
@@ -69,8 +74,8 @@ def main():
                         print("Multi Club Members cannot be checked in individually. ")
                     break
             else:
-                print("Member not found or ineligible for check-in.")
-                       
+                print("Member not found or ineligible for check-in.")  
+            print()          
         elif choice == 5: #generate bill
             member_id = input("Enter Member ID to generate bill: ")
             billed_member =  None
@@ -86,11 +91,13 @@ def main():
                     print("$50")
             else: 
                 print("Member not found or ineligible for check-in.")
+            print()
         elif choice == 6: #exit
             print("Exiting  ")
             break
         else: 
             print("Invalid choice. Please select again.")
+        print()
 
 if __name__== "__main__":
     main()
