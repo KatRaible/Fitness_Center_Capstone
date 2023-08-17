1556493880c761e8651dc497354217d40b5ed0e3
Adriana Jeffries <adrianamjeffries@gmail.com>
2023-08-16 20:35:59 -0400

    Step 4

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
                 members.append(MultiClubMember(member_id, name))
             else:
                 print("error")
-         elif choice == 2:  # remove member
+        elif choice == 2:  # remove member
             member_id = input("Enter member ID to remove: ")
             for member in members:
                 if member.member_id == member_id:
@@ -57,6 +57,18 @@ def main():
             else:
                 print("Member not found.")
         elif choice == 4: #check in member
+            member_id = input("Enter member ID to check in: ")
+            for member in members:
+                if member.member_id == member_id:
+                    if isinstance(member, Single_Club_Member):
+                        member.club.check_in_member(member)
+                        print(f"Member {member.name} checked in at {member.club.name}")
+                    elif isinstance(member, MultiClubMember):
+                        print("Multi Club Members cannot be checked in individually. ")
+                    break
+            else:
+                print("Member not found or ineligible for check-in.")
+                       
             pass
         elif choice == 5: #generate bill
             pass
